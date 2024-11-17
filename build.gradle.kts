0664e068ae0b97822d9267c35172ea8185e478f7
ZayadMaiyaki <zmaiy092@uottawa.ca>
2024-11-17 12:53:09 -0500

    initial coomit

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 kotlin {
